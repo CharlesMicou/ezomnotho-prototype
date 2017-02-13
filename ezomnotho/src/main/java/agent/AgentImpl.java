4fddb006608909ce018efd5a1a3c84b8d671fcdc
CharlesMicou <charlesmicou@gmail.com>
2017-02-13 02:51:38 +0000

    wip

--- ezomnotho/src/main/java/agent/AgentImpl.java
+++ ezomnotho/src/main/java/agent/AgentImpl.java
@@ -5,7 +5,19 @@ import market.TradeOffer;
 import market.TradeResponse;
 import market.TradeResult;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class AgentImpl implements Agent {
+
+    private ConcurrentHashMap<Integer, Integer> ownedGoods;
+    private double ownedMoney;
+
+    AgentImpl(double initialMoney){
+        this.ownedMoney = initialMoney;
+        this.ownedGoods = new ConcurrentHashMap<>();
+    };
+
+
     @Override
     public ImmutableList<TradeOffer> createTradeOffers() {
         return null;
@@ -23,11 +35,13 @@ public class AgentImpl implements Agent {
 
     @Override
     public void sellGoodsTo(int goodId, int quantity, double pricePerItem) {
-
+        ownedGoods.compute(goodId, (k, v) -> v == null ? quantity : v + quantity);
+        ownedMoney -= pricePerItem * quantity;
     }
 
     @Override
     public void buyGoodsFrom(int goodId, int quantity, double pricePerItem) {
-
+        ownedGoods.compute(goodId, (k, v) -> v == null ? 0 : v + quantity);
+        ownedMoney -= pricePerItem * quantity;
     }
 }

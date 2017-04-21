433a97a691c4498be0d9df2ff42f58268e5fe709
Charlie Micou <charlie@improbable.io>
2017-04-21 19:41:13 +0100

    added which strategies to make basic strategies for

--- ezomnotho/src/main/java/agent/valuation/BasicValuationStrategyFactory.java
+++ ezomnotho/src/main/java/agent/valuation/BasicValuationStrategyFactory.java
@@ -9,4 +9,11 @@ public class BasicValuationStrategyFactory implements ValuationStrategyFactory {
     public ValuationStrategy makeStrategyFor(GoodId goodId) {
         return null;
     }
+
+    // basic strategies to implement:
+    // simple learned variance gaussian: historic mean and variance
+    // simple gaussian: historic mean, fixed fraction variance
+    // simple smoothed mean gaussian: historic expo-smoothed mean, fixed fraction variance
+    // simple unmet trades: unmnet trades augmented mean, learned variance
+    // learned unmet trades:
 }
